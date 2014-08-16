3adce4bf762a03f6ffa62adb09e11f68b6463164
Inyang Umoh <inyang@axumtechnologies.com>
2014-08-16 12:32:29 -0700

    tweaked configs to work on gce

--- tomcat-standalone/roles/tomcat/files/tomcat-initscript.sh
+++ tomcat-standalone/roles/tomcat/files/tomcat-initscript.sh
@@ -15,7 +15,7 @@
 #
  
 #Location of JAVA_HOME (bin files)
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
  
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH
